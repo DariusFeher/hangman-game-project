b262aa78bad7f92c8b69904bf955d0bbcff853ce
Darius Feher <darius.feher@student.manchester.ac.uk>
2021-03-30 02:18:19 +0300

    Fix spacing

--- js/hangman_script.js
+++ js/hangman_script.js
@@ -96,9 +96,9 @@
 
 	}
 
-	function setCharAt(str,index,chr) {
-	    if(index > str.length-1) return str;
-	    return str.substring(0,index) + chr + str.substring(index+1);
+	function setCharAt(str, index, chr) {
+	    if (index > str.length - 1) return str;
+	    return str.substring(0, index) + chr + str.substring(index + 1);
 	}
 
 
